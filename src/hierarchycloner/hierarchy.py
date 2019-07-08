beddfedaddad40070f31b52c1229122d866adc03
Florian Kempenich <Flori@nKempenich.com>
2019-07-08 18:46:39 +0100

    Implement Hierarchy loader

---
+++ src/hierarchycloner/hierarchy.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+import yaml
+
+
+class RepoToClone:
+    url: str
+    path: Path
+    name: str
+
+    def __init__(self, url: str, path: Path, name: str = None):
+        self.url = url
+        self.path = path
+        if name:
+            self.name = name
+        else:
+            self.name = self._extract_name_from_url()
+
+    def _extract_name_from_url(self):
+        return self.url.split('/')[-1].replace('.git', '')
+
+    def __repr__(self) -> str:
+        return f'<url: {self.url} | path: {self.path} | name: {self.name}>'
+
+    def __eq__(self, other):
+        return self.url == other.url and self.path == other.path and self.name == other.name
+
+
+def load_from_file(hierarchy_file: Path):
+    if not hierarchy_file.exists():
+        raise ValueError('File does not exist!')
+
+    hierarchy_yaml = yaml.safe_load(hierarchy_file.read_text())
+    print(hierarchy_yaml)
+
+    hierarchy = []
+    for repo_yaml in hierarchy_yaml:
+        if 'url' not in repo_yaml or 'path' not in repo_yaml:
+            raise ValueError('Invalid Hierarchy. Please check Hierarchy file!')
+
+        repo_to_clone = RepoToClone(repo_yaml['url'], Path(repo_yaml['path']), repo_yaml.get('name'))
+        hierarchy.append(repo_to_clone)
+
+    return hierarchy
---
+++ tests/hierarchycloner/test_cloner.py
@@ -0,0 +1,69 @@
+from pathlib import Path
+
+import pytest
+from pytest import fixture
+
+from src.hierarchycloner import hierarchy
+from src.hierarchycloner.hierarchy import RepoToClone
+
+
+@fixture
+def tmp_file(tmp_path):
+    return tmp_path / 'tmp_file.yaml'
+
+
+class TestLoadHierarchy:
+    def test_file_does_not_exist__raise(self):
+        doesnt_exist = Path('/does/not/exist')
+        assert not doesnt_exist.exists()
+
+        with pytest.raises(ValueError, match='File does not exist!') as e:
+            hierarchy.load_from_file(doesnt_exist)
+
+    def test_file_invalid__raise(self, tmp_file):
+        tmp_file.write_text('''
+        { 'this': is not yaml}
+        ''')
+
+        with pytest.raises(ValueError, match='Invalid Hierarchy') as e:
+            hierarchy.load_from_file(tmp_file)
+
+    def test_file_invalid_2__raise(self, tmp_file):
+        tmp_file.write_text('''
+        - path: '/this/is/a/path'
+          name: hc
+        ''')
+
+        with pytest.raises(ValueError, match='Invalid Hierarchy') as e:
+            hierarchy.load_from_file(tmp_file)
+
+    def test_valid_hierarchy(self, tmp_file: Path):
+        tmp_file.write_text('''
+        - url: 'git@github.com:FlorianKempenich/Hierarchy-Cloner.git'
+          path: '/this/is/a/path'
+          name: hc
+          
+        - url: 'git@github.com:FlorianKempenich/kata.git'
+          path: '/some/other/path'
+          name: kata
+        ''')
+
+        repos = hierarchy.load_from_file(tmp_file)
+
+        assert repos == [
+                RepoToClone(url='git@github.com:FlorianKempenich/Hierarchy-Cloner.git',
+                            path=Path('/this/is/a/path'),
+                            name='hc'),
+                RepoToClone(url='git@github.com:FlorianKempenich/kata.git',
+                            path=Path('/some/other/path'),
+                            name='kata')]
+
+    def test_no_explicit_name__extract_from_url(self, tmp_file):
+        tmp_file.write_text('''
+        - url: 'git@github.com:FlorianKempenich/Hierarchy-Cloner.git'
+          path: '/this/is/a/path'
+        ''')
+
+        repos = hierarchy.load_from_file(tmp_file)
+
+        assert repos[0].name == 'Hierarchy-Cloner'
